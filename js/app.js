95a419bc9ffddd0c345f2c6990a9d90700e9914e
arielon <43272158+arielon@users.noreply.github.com>
2019-07-03 22:09:06 -0500

    Actualizado de archivo con todas las funciones y documentación

--- js/app.js
+++ js/app.js
@@ -4,192 +4,186 @@ $(function () {
 	changeColors()
 
 	function changeColors() {
-		var titulo = $(".main-titulo");
-		var color = titulo.data("color") === undefined ? 0 : titulo.data("color") + 1;
+		var titulo = $(".main-titulo")
+		var color = titulo.data("color") === undefined ? 0 : titulo.data("color") + 1
 		titulo.data("color", color % colors.length).delay(800).animate({
 			color: colors[titulo.data("color")]
 		}, 800, changeColors)
 	}
 
-	//Iniciar botón
+	//2. Llenado de dulces aleatorios
+	function elementos() {
+		for (var i = 0; i <= 7; i++) {
+			for (var j = 0; j < 7; j++) {
+				var numero = Math.floor((Math.random() * 4) + 1)
+				$(".col-" + i).append("<img class= 'elemento' src='image/" + numero + ".png'>")
+			}
+		}
+		$('.elemento').css("cursor", "move")
+		buscarCoincidencia()
+	}
+
+	function Columnas() {
+		var ac = false
+		for (var c = 1; c <= 7; c++) {
+			for (var f = 1; f <= 7; f++) {
+				imagen1 = $(".col-" + c).children("img:nth-child(" + f + ")").attr("src")
+				imagen2 = $(".col-" + c).children("img:nth-child(" + (f + 1) + ")").attr("src")
+				imagen3 = $(".col-" + c).children("img:nth-child(" + (f + 2) + ")").attr("src")
+				if (imagen1 == imagen2 && imagen2 == imagen3) {
+					$(".col-" + c).children("img:nth-child(" + f + ")").attr("class", "elemento igual")
+					$(".col-" + c).children("img:nth-child(" + (f + 1) + ")").attr("class", "elemento igual")
+					$(".col-" + c).children("img:nth-child(" + (f + 2) + ")").attr("class", "elemento igual")
+					ac = true
+				}
+			}
+		}
+		return ac
+	}
+
+	function Filas() {
+		var af = false
+		for (var f = 1; f <= 7; f++) {
+			for (var c = 1; c <= 7; c++) {
+				imagen1 = $(".col-" + f).children("img:nth-child(" + c + ")").attr("src")
+				imagen2 = $(".col-" + (f + 1)).children("img:nth-child(" + c + ")").attr("src")
+				imagen3 = $(".col-" + (f + 2)).children("img:nth-child(" + c + ")").attr("src")
+				if (imagen1 == imagen2 && imagen2 == imagen3) {
+					$(".col-" + f).children("img:nth-child(" + c + ")").attr("class", "elemento igual")
+					$(".col-" + (f + 1)).children("img:nth-child(" + c + ")").attr("class", "elemento igual")
+					$(".col-" + (f + 2)).children("img:nth-child(" + c + ")").attr("class", "elemento igual")
+					af = true
+				}
+			}
+		}
+		return af
+	}
+
+	function buscarCoincidencia() {
+		anf = Filas()
+		anc = Columnas()
+		if (anf == true || anc == true) {
+			BorrarDulces()
+		} else {
+			MoverDulces()
+		}
+	}
+
+	//3. Animación y puntaje del juego
+	function BorrarDulces() {
+		$(".igual").effect('pulsate', 1000)
+		$(".igual").animate({
+			opacity: '0'
+		}, {
+			duration: 800
+		}).animate({
+			opacity: '0'
+		}, {
+			duration: 400,
+			complete: function () {
+				$(this).remove()
+				scorePoints()
+				setTimeout(DulcesNuevos, 200)
+			},
+			queue: true
+		})
+
+		function scorePoints() {
+			if ($('#timer').text() != '00:00') {
+				puntos = parseInt($('#score-text').text()) + 10;
+				$('#score-text').text(puntos)
+			}
+		}
+	}
+
+	function DulcesNuevos() {
+		for (var c = 1; c <= 7; c++) {
+			for (var f = 1; f <= 7; f++) {
+				if ($(".col-" + f).children("img:nth-child(" + c + ")").html() == null) {
+					var numero = Math.floor((Math.random() * 4) + 1)
+					$(".col-" + f).prepend("<img class= 'elemento' src='image/" + numero + ".png'>")
+				}
+			}
+		}
+		$('.elemento').css("cursor", "move")
+		buscarCoincidencia()
+	}
+
+	//7. Mover dulces Drag & Drop
+	var MovimientoTotal = 0
+
+	function MoverDulces() {
+		var dulce1
+		var dulce2
+		var dulceSrc1
+		var dulceSrc2
+		$("img").draggable({
+			revert: true,
+			containment: ".panel-tablero",
+			revertDuration: 500,
+			grid: [104, 104],
+			zIndex: 10,
+			drag: constrainCandyMovement,
+			start: function () {
+				dulce1 = this
+				dulceSrc1 = $(this).attr("src")
+			}
+		})
+		// 5. Anotación de movimientos
+		$("img").droppable({
+			drop: function () {
+				dulce2 = this
+				dulceSrc2 = $(this).attr("src")
+				$(dulce2).attr("src", dulceSrc1)
+				$(dulce1).attr("src", dulceSrc2)
+				MovimientoTotal = MovimientoTotal + 1
+				$("#movimientos-text").html(MovimientoTotal)
+				setTimeout(buscarCoincidencia, 400)
+			}
+		})
+	}
+
+	function constrainCandyMovement(event, candyDrag) {
+		candyDrag.position.top = Math.min(100, candyDrag.position.top);
+		candyDrag.position.bottom = Math.min(100, candyDrag.position.bottom);
+		candyDrag.position.left = Math.min(105, candyDrag.position.left);
+		candyDrag.position.right = Math.min(105, candyDrag.position.right);
+	}
+
+	//4. Temporizador
+	function startTimer(duration, display) {
+		var timer = duration,
+			minutes, seconds
+		setInterval(function () {
+			minutes = parseInt(timer / 60, 10)
+			seconds = parseInt(timer % 60, 10)
+
+			minutes = minutes < 10 ? "0" + minutes : minutes
+			seconds = seconds < 10 ? "0" + seconds : seconds
+
+			display.text(minutes + ":" + seconds)
+
+			if (--timer < 0) {
+				timer = 00
+				$(".panel-tablero").remove()
+				$(".panel-score ").animate({
+					width: "100%",
+					padding: "0px 12.5%"
+				})
+			}
+		}, 1000)
+	}
+	var fiveMinutes = 60 * 2,
+		display = $('#timer')
+	//6. Botón Iniciar y Reiniciar
 	$(".btn-reinicio").click(function () {
 		if ($(this).text() === "Iniciar") {
 			$(this).text("Reiniciar")
-			crearFila()
-			detectarColumna()
+			elementos()
+			startTimer(fiveMinutes, display)
 		} else {
-			$(this).text("Iniciar")
+			location.reload()
 		}
 	})
 
-	//2. Agregar dulces
-	function crearFila() {
-		for (i = 0; i < 7; i++) {
-			$(".panel-tablero > div").append("<img src='' class='elemento'/>")
-			$(".elemento").attr("src", function () {
-				var numero = Math.floor((Math.random() * 4) + 1)
-				var ruta = "image/" + numero + ".png"
-				return ruta
-			})
-		}
-	}
-
-	function detectarColumna() {
-		var columna1 = $(".col-1").children()
-
-		var matrizElementos = []
-		for (var i = 0; i < 7; i++) {
-			var dulcesColumna = []
-			$(columna1).each(function () {
-				dulcesColumna.push($(this))
-			})
-			matrizElementos.push(dulcesColumna)
-		}
-
-		var dulce1 = matrizElementos[0][0]
-		var dulce2 = matrizElementos[0][1]
-		var dulce3 = matrizElementos[0][2]
-		var dulce4 = matrizElementos[0][3]
-		var dulce5 = matrizElementos[0][4]
-		var dulce6 = matrizElementos[0][5]
-		var dulce7 = matrizElementos[0][6]
-
-		var dulceC1 = matrizElementos[0][0].attr("src")
-		var dulceC2 = matrizElementos[0][1].attr("src")
-		var dulceC3 = matrizElementos[0][2].attr("src")
-		var dulceC4 = matrizElementos[0][3].attr("src")
-		var dulceC5 = matrizElementos[0][4].attr("src")
-		var dulceC6 = matrizElementos[0][5].attr("src")
-		var dulceC7 = matrizElementos[0][6].attr("src")
-
-
-		if (dulceC1 == dulceC2 && dulceC2 == dulceC3) {
-			$([dulce1, dulce2, dulce3]).each(function() {
-				$(this).effect('pulsate', 1000)
-				$(this).animate({
-					opacity: '0'
-				}, {
-					duration: 800
-				}).animate({
-					opacity: '0'
-				})
-			})
-		}
-		if (dulceC2 == dulceC3 && dulceC3 == dulceC4) {
-			$([dulce2, dulce3, dulce4]).each(function() {
-				$(this).effect('pulsate', 1000)
-				$(this).animate({
-					opacity: '0'
-				}, {
-					duration: 800
-				}).animate({
-					opacity: '0'
-				})
-			})
-		}
-		if (dulceC3 == dulceC4 && dulceC4 == dulceC5) {
-			$([dulce3, dulce4, dulce5]).each(function() {
-				$(this).effect('pulsate', 1000)
-				$(this).animate({
-					opacity: '0'
-				}, {
-					duration: 800
-				}).animate({
-					opacity: '0'
-				})
-			})
-		}
-		if (dulceC4 == dulceC5 && dulceC5 == dulceC6) {
-			$([dulce4, dulce5, dulce6]).each(function() {
-				$(this).effect('pulsate', 1000)
-				$(this).animate({
-					opacity: '0'
-				}, {
-					duration: 800
-				}).animate({
-					opacity: '0'
-				})
-			})
-		}
-		if (dulceC5 == dulceC6 && dulceC6 == dulceC7) {
-			$([dulce5, dulce6, dulce7]).each(function() {
-				$(this).effect('pulsate', 1000)
-				$(this).animate({
-					opacity: '0'
-				}, {
-					duration: 800
-				}).animate({
-					opacity: '0'
-				})
-			})
-		}
-
-		for (let i = 0; i < 1; i++) {
-			for (var j = 0; j < 5; j++) {
-				//y_itemAnterior = matrizElementos[i][j].substr(6, 1)
-				//y_itemActual = matrizElementos[i][j + 1].substr(6, 1)
-				//y_itemSiguiente = matrizElementos[i][j + 2].substr(6, 1)
-
-				/*
-				if (y_itemActual == "1") {
-					var dulce1 = 1
-				}
-				if (y_itemActual == "2") {
-					var dulce2 = 2
-				}
-				if (y_itemActual == "3") {
-					var dulce3 = 3
-				}
-				if (y_itemActual == "4") {
-					var dulce4 = 4
-				}
-				if (y_itemAnterior == "1") {
-					var prevdulce1 = 1
-				}
-				if (y_itemAnterior == "2") {
-					var prevdulce2 = 2
-				}
-				if (y_itemAnterior == "3") {
-					var prevdulce3 = 3
-				}
-				if (y_itemAnterior == "4") {
-					var prevdulce4 = 4
-				}
-				if (y_itemSiguiente == "1") {
-					var postdulce1 = 1
-				}
-				if (y_itemSiguiente == "2") {
-					var postdulce2 = 2
-				}
-				if (y_itemSiguiente == "3") {
-					var postdulce3 = 3
-				}
-				if (y_itemSiguiente == "4") {
-					var postdulce4 = 4
-				}
-				*/
-				//var coincidencia1 = (dulce1 == prevdulce1 && dulce1 == postdulce1)
-
-				/*
-				var productIds = {};
-				$('.list').each(function () {
-					var prodId = $(this).attr('data-productid');
-					if (productIds[prodId]) {
-						$(this).remove();
-					} else {
-						productIds[prodId] = true;
-					}
-				})
-				*/
-			}
-		}
-
-		/*
-		$("#att").click(function () {
-			dulce6.hide("drop", {
-				direction: "down"
-			}, "slow")
-		})
-		*/
-	}
 })
